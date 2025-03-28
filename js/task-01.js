fd0eaad29fa871b1054354ae1efe3b1818c1fd11
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-03-28 07:40:08 +0200

    1-3

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,15 @@
 const categories = document.querySelector("#categories");
-console.log(categories);
+// console.log(categories);
+const listItems = categories.children;
+console.log("Number of categories:", listItems.length);
+// console.log([...listItems]);
+
+[...listItems].forEach((item) => {
+  console.log("Category:", item.firstElementChild.textContent);
+  //   console.log(item);
+  console.log("Elements:", item.lastElementChild.children.length);
+  //   console.log("Elements:", item.lastElementChild.length);
+  //   const elements = document.querySelectorAll("ul > li");
+  //   console.log(elements.length);
+  //   console.log("Elements:", elements.length);
+});
