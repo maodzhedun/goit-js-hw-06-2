3a9363d01f79a7304fef3919dbb30bf4f73c4452
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-04-06 18:45:50 +0300

    to 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,50 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsForm = document.getElementById("controls");
+// console.dir(controlsForm);
+const divCubes = document.getElementById("boxes");
+// console.log(divCubes);
+const createBtn = document.querySelector("[data-create]");
+// console.log(createBtn);
+const destroyBtn = document.querySelector("[data-destroy]");
+// console.log(destroyBtn);
+const inputVal = document.querySelector("input");
+// console.log(inputVal);
+
+createBtn.addEventListener("click", onCreateClick);
+
+function onCreateClick() {
+  divCubes.innerHTML = "";
+  const amount = Number(inputVal.value);
+  console.log(amount);
+  if (amount > 0 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert("Введіть число від 1 до 100");
+  }
+}
+
+destroyBtn.addEventListener("click", onDestroyClick);
+
+function onDestroyClick() {
+  divCubes.innerHTML = "";
+  inputVal.value = "";
+}
+
+function createBoxes(amount) {
+  let boxSize = 30; // Розмір кожного наступного блока
+
+  for (let i = 1; i <= amount; i += 1) {
+    {
+      const box = document.createElement("div");
+      box.style.width = `${boxSize}px`;
+      box.style.height = `${boxSize}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      box.classList.add("box");
+      divCubes.appendChild(box);
+    }
+    boxSize += 10;
+  }
+}
